bd2180677d4bc942376d5622542a18408b51b7d3
haddybhaiya <harshsharma2k614@gmail.com>
2026-03-28 17:31:46 +0530

    update : error catching & stricting comm size

--- agent/llm/test_generator.py
+++ agent/llm/test_generator.py
@@ -2,30 +2,39 @@ from agent.llm.groq_client import client, model
 def generate_tests(diff:str,context:list =None)->str:
     context_text = ""
     if context:
-        context_text = "\n\nContext:\n" + "\n".join(context[:5])  # Include only the first 5 chunks for context
+        context_text = "\n\n".join(context[:5])  # Include only the first 5 chunks for context
     prompt = f"""
     You are a senior software engineer.
-    Generate unit tests for the given code diff.
+    Generate concise pytest test cases for the given code diff.
+
+    STRICT RULES:
+    - Output only code
+    - Max  5 test functions
+    - keep tests short
+    - Focus on edge cases and core logic
+    - NO explanations, NO extra text
+
 
     ---CONTEXT---
     {context_text}
 
     ---DIFF---
     {diff}
-
-    ---OUTPUT FORMAT---
-    -Use pytest
-    - cover edge cases
-    - keep it concise
     """
-    response = client.chat.completions.create(
-        model = model,
-        messages = [
-            
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-      )
-    return response.choices[0].message.content
+
+
+    try:
+        response = client.chat.completions.create(
+            model = model,
+            messages = [
+                
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+          )
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        return f"Error generating tests:{str(e)}"
+    
