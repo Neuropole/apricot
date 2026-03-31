b8bc9dd67c44ce037ab56714be21125d905d9b63
haddybhaiya <harshsharma2k614@gmail.com>
2026-03-31 17:09:21 +0530

    fix : fetch head Pr branch

--- agent/github/committer.py
+++ agent/github/committer.py
@@ -1,18 +1,19 @@
+import os
 import subprocess
+
 def commit_tests():
     try:
-        # configue git (required in github actions)
-        subprocess.run(['git','config',"--global","user.name","github-actions"],check=True)
-        subprocess.run(["git","config","--global","user.email","github-actions@github.com"],
-                        check=True
-                        )
+        branch = os.getenv("GITHUB_HEAD_REF")  # PR branch
 
-        # add the generated test file
-        subprocess.run(["git","add","tests/test_generated.py"],check=True)
-        # commit the changes
-        subprocess.run(["git","commit","-m","Add AI-generated tests"],check=True)
-        # push
-        subprocess.run(["git","push"],check=True)
-        print("Tests committed and pushed successfully.")
+        subprocess.run(["git", "config", "user.name", "github-actions"], check=True)
+        subprocess.run(["git", "config", "user.email", "actions@github.com"], check=True)
+
+        subprocess.run(["git", "add", "tests/"], check=True)
+        subprocess.run(["git", "commit", "-m", "Add AI-generated tests"], check=True)
+
+        subprocess.run(["git", "push", "origin", f"HEAD:{branch}"], check=True)
+
+        print("Tests pushed to PR branch")
+
     except Exception as e:
-        print(f"Commit Failed {e}")
+        print("Commit Failed", e)
